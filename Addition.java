10982960270fb673a6c1744d769cfdc14124d352
makarandhinge <mmh2212002@gmail.com>
2023-03-02 07:01:57 +0530

    First Java program initated

---
+++ Addition.java
@@ -0,0 +1,10 @@
+class Addition
+{
+    public static void main(String args[])
+    {
+        int a=10,b=20,c=30;
+        int sum;
+        sum=a+b+c;
+        System.out.println("Addition of three no are " + sum);
+    }
+}
