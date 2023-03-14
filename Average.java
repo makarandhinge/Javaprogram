705d259607ee4ed1ce85c8449fb1bd05d78a1862
makarandhinge <mmh2212002@gmail.com>
2023-03-14 23:06:04 +0530

    _250+_program

---
+++ Average.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Average {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Check average of 5 number");
+    System.out.println("Enter any 5 number");
+    int a = sc.nextInt();      
+    int b = sc.nextInt();  
+    int c = sc.nextInt();  
+    int d = sc.nextInt();  
+    int e = sc.nextInt();
+    int avg;
+    avg = (a + b + c + d + e)/5;
+    System.out.println("Average of given number is " + avg);   
+    sc.close();
+    }
+    
+}
