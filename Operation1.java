dc761f3fbcb5660c8367fea5e3fa9dfac6968084
makarandhinge <mmh2212002@gmail.com>
2023-03-16 06:33:51 +0530

    Argument passing in java call by value passing object as a value

---
+++ Operation1.java
@@ -0,0 +1,13 @@
+public class Operation1 {
+    int data = 50;
+    void change(Operation1 o)
+    {
+        o.data = o.data + 100;
+    }
+    public static void main(String[] args) {
+        Operation1 op = new Operation1();
+        System.out.println("before change " + op.data);
+        op.change(op);
+        System.out.println("after change " + op.data);
+    }
+}
