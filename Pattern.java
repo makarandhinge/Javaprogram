9b0e4f5ae6d8bee9e974bb2ee71e910bae722394
makarandhinge <mmh2212002@gmail.com>
2023-04-17 22:54:47 +0530

    Three Star Pattern problem are build by self

--- Pattern.java
+++ Pattern.java
@@ -1,2 +1,63 @@
-package PACKAGE_NAME;public class Pattern {
+import java.util.*;
+
+public class Pattern {
+
+
+    void star_01_pattern(int n){
+
+        for (int rows = 1; rows <= n; rows++) {
+            for (int col = 1; col <= rows ; col++) {
+                System.out.print("* ");
+
+            }
+            System.out.println();
+
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        Pattern p = new Pattern();
+        System.out.println("Enter nth value below");
+        n = sc.nextInt();
+        p.star_01_pattern(n);
+        p.star_02_pattern(n);
+        p.star_03_pattern(n);
+
+    }
+
+    void star_02_pattern(int n){
+
+        for (int rows = n; rows >= 1 ; rows--) {
+            for (int col = rows; col >= 1 ; col--) {
+                System.out.print("* ");
+
+            }
+            System.out.println();
+
+        }
+        System.out.println();
+    }
+
+    void star_03_pattern(int n){
+        int m = 1;
+        int k = n - 1;
+        for (int rows = 1; rows <= n ; rows++) {
+            for (int col = 1; col <= k ; col++) {
+                System.out.print("  ");
+
+            }
+            for (int star = 1; star <= m ; star++) {
+                System.out.print("* ");
+            }
+            m = m + 2;
+            k = k - 1;
+            System.out.println();
+
+
+
+        }
+    }
 }
