5457da2a22636c336d53fbd2c2d391666a4e99ac
makarandhinge <mmh2212002@gmail.com>
2023-04-22 11:39:05 +0530

    Started using Eclipse and build first program

---
+++ Eclipse Workspace/src/HelloWorld.java
@@ -0,0 +1,10 @@
+//First Program in Eclipse
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
+	}
+
+}
